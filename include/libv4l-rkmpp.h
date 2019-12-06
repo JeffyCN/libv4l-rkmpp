14a0e979f2427b4e27a1a4df616206d90ba0c161
Jeffy Chen <jeffy.chen@rock-chips.com>
2019-12-06 09:47:48 +0800

    Release 1.2.1~20191206
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- include/libv4l-rkmpp.h
+++ include/libv4l-rkmpp.h
@@ -37,7 +37,7 @@
 #define V4L2_PIX_FMT_VP9_FRAME v4l2_fourcc('V', 'P', '9', 'F')
 #endif
 
-#define LIBV4L_RKMPP_VERSION "1.2.0~20191016"
+#define LIBV4L_RKMPP_VERSION "1.2.1~20191206"
 
 extern int rkmpp_log_level;
 
